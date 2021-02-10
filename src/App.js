0b0f63923ace86b19bdbe9a13c1edca6b89711f3
Tony <tony.d.nguyen@protonmail.com>
2021-02-10 13:48:33 -0600

    Sucessfully accessed Metals API.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
-import Metals from "./components/Metals";
+import React, { useState } from "react";
 
 const App = () => {
+  const metalsAPI = {
+    key: "bd8x58t67kikf2jueqzeex2g0hpysu9rbzlgm9vbu3of34yhla013s04dz3r",
+    base: "https://www.metals-api.com/api/latest",
+    currency: "USD",
+    metals: "XAU,XAG",
+  };
+
+  const [metalPrices, setMetalPrices] = useState({});
+
+  const fetchAPI = () => {
+    fetch(
+      `${metalsAPI.base}?access_key=${metalsAPI.key}&base=${metalsAPI.currency}&${metalsAPI.metals}`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setMetalPrices(result);
+        console.log(result);
+      });
+  };
+
+  const Metals = () => {
+    return (
+      <div>
+        <ul>
+          <li>Gold:</li>
+          <li>Silver:</li>
+          <button onClick={fetchAPI}>Get Price</button>
+        </ul>
+      </div>
+    );
+  };
+
   return (
     <div>
       <h1>Current Price of Gold & Silver</h1>
