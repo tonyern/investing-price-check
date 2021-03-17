612775892b2d5ab73083f31dee0e3fe380dd97f4
Tony <tony.d.nguyen@protonmail.com>
2021-03-17 10:22:57 -0500

    Created seperate components for asset checker.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import AssetPriceChecker from "./components/assetPriceChecker";
 
 const App = () => {
-  const metalsAPI = {
-    key: "bd8x58t67kikf2jueqzeex2g0hpysu9rbzlgm9vbu3of34yhla013s04dz3r",
-    base: "https://www.metals-api.com/api/latest",
-    currency: "USD",
-    metals: "XAU,XAG",
-  };
-
-  const [silverPrice, setSilverPrice] = useState("---");
-  const [goldPrice, setGoldPrice] = useState("---");
-
-  const fetchAPI = () => {
-    axios
-      .get(
-        `${metalsAPI.base}?access_key=${metalsAPI.key}&base=${metalsAPI.currency}&${metalsAPI.metals}`
-      )
-      .then((response) => {
-        console.log(response.data);
-        setGoldPrice(Math.round(1 / response.data.rates.XAU));
-        setSilverPrice(Math.round(1 / response.data.rates.XAG));
-      })
-      .catch((error) => {
-        if (error.response) {
-          // Error if error in retrieving data.
-          console.log(error.response.data);
-        } else if (error.request) {
-          // Error if no response was received.
-          console.log(error.request);
-        } else {
-          // Other errors.
-          console.log(error.message);
-        }
-      });
-  };
-
-  return (
-    <div>
-      <h1>Current Price of Gold & Silver</h1>
-      <ul>
-        <li>Gold: ${goldPrice}</li>
-        <li>Silver: ${silverPrice}</li>
-        <button onClick={fetchAPI}>Get Price</button>
-      </ul>
-    </div>
-  );
+  return <AssetPriceChecker />;
 };
 
 export default App;
